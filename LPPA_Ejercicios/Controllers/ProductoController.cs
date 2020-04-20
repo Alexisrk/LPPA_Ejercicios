252f651fee52ea62a07388885ba32a1e7823752e
Alexis <alexisrk5@gmail.com>
2020-04-20 20:21:03 -0300

    .- Initial Version.

---
+++ LPPA_Ejercicios/Controllers/ProductoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA_Ejercicios.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
